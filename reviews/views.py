75699a0f1d53b02aabaf162688c4756ebeff395e
Alonso Parra <alienx9889@gmail.com>
2016-07-10 22:47:24 -0400

    Adding Form

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from .forms import ReviewForm
 from .models import Review, Wine
+
+import datetime
+
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -24,3 +32,24 @@ def wine_list(request):
 def wine_detail(request, wine_id):
     wine = get_object_or_404(Wine, pk=wine_id)
     return render(request, 'reviews/wine_detail.html', {'wine': wine})
+
+@login_required
+def add_review(request, wine_id):
+    wine = get_object_or_404(Wine, pk=wine_id)
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+        rating = form.cleaned_data['rating']
+        comment = form.cleaned_data['comment']
+        user_name = request.user.username
+        review = Review()
+        review.wine = wine
+        review.user_name = user_name
+        review.rating = rating
+        review.comment = comment
+        review.pub_date = datetime.datetime.now()
+        review.save()
+        update_clusters()
+        return HttpResponseRedirect(reverse('reviews:wine_detail', args=(wine.id,)))
+
+    return render(request, 'reviews/wine_detail.html',{'wine':wine, 'form': form})
+
